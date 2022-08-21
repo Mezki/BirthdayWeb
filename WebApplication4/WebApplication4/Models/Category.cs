f4553eaf82591562177e92539961d476fee7d70b
Mezki <egorov.sev@gmail.com>
2022-08-21 18:54:03 +0300

    init comment

---
+++ WebApplication4/WebApplication4/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }   
+        public DateTime CreatedDate { get; set; }  = DateTime.Now;
+        [Required]
+        public DateTime BirthDate { get; set; }
+    }
+}
